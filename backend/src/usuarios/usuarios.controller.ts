582435253406670c6aca284e17d9b4f5a0773b86
VictorMA18 <vmamanian@unsa.edu.pe>
2025-10-24 13:30:38 -0500

    ajusto protección y validaciones en usuarios.controller

--- backend/src/usuarios/usuarios.controller.ts
+++ backend/src/usuarios/usuarios.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, NotFoundException, UseGuards } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
+  @Roles("administrador")
   @Post()
   async create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return await this.usuariosService.create(createUsuarioDto);
@@ -30,11 +35,13 @@ export class UsuariosController {
     return usuario;
   }
 
+  @Roles("administrador")
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     return await this.usuariosService.update(+id, updateUsuarioDto);
   }
 
+  @Roles("administrador")
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.usuariosService.remove(+id);
