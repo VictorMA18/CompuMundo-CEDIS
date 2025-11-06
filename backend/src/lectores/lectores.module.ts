59304043ed494de6fc6d5931042c4478128f438e
VictorMA18 <vmamanian@unsa.edu.pe>
2025-11-06 09:29:06 -0500

    Agregando y actualizando módulo de lectores

--- backend/src/lectores/lectores.module.ts
+++ backend/src/lectores/lectores.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LectoresService } from './lectores.service';
 import { LectoresController } from './lectores.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [LectoresController],
   providers: [LectoresService],
 })
