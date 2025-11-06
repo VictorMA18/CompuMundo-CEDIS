bd7fde4f580b27d9b81652e7fd4874cb8fa37201
VictorMA18 <vmamanian@unsa.edu.pe>
2025-11-06 09:30:29 -0500

    Eliminando y actualizando DTO antiguo update-lectore

--- backend/src/lectores/dto/update-lectore.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLectoreDto } from './create-lectore.dto';
-
-export class UpdateLectoreDto extends PartialType(CreateLectoreDto) {}
