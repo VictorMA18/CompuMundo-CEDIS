298fe74c608ba1722f2fbb989441b1d8277ff080
VictorMA18 <vmamanian@unsa.edu.pe>
2025-11-13 20:35:25 -0500

    Agregando el módulo de autor

---
+++ backend/src/autor/autor.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AutorService } from './autor.service';
+import { AutorController } from './autor.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [AutorService],
+  controllers: [AutorController]
+})
+export class AutorModule {}
