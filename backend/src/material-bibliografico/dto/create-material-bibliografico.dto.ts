ca5f3ac64a15a7f5787fc4c09b1fcb94eb777d78
VictorMA18 <vmamanian@unsa.edu.pe>
2025-11-18 00:48:35 -0500

    Ajustando DTO de material bibliográfico para validaciones y campos opcionales

--- backend/src/material-bibliografico/dto/create-material-bibliografico.dto.ts
+++ backend/src/material-bibliografico/dto/create-material-bibliografico.dto.ts
@@ -21,7 +21,8 @@ export class CreateMaterialBibliograficoDto {
   CatId: number;
 
   @IsEnum(FormatoMaterial)
-  MatBibFor: FormatoMaterial;
+  @IsOptional()
+  MatBibFor?: FormatoMaterial;
 
   @IsOptional()
   @Matches(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/, {
