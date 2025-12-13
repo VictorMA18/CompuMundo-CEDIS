69a8c34cc1151abbe7a3b1eacc9dc05b3557ebad
VictorMA18 <vmamanian@unsa.edu.pe>
2025-12-13 18:24:02 -0500

    Documentando y ajustando categorías

--- backend/src/categorias/categorias.module.ts
+++ backend/src/categorias/categorias.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   imports: [PrismaModule],
   controllers: [CategoriasController],
   providers: [CategoriasService],
+  exports: [CategoriasService]
 })
 export class CategoriasModule {}
--- backend/src/categorias/dto/create-categoria.dto.ts
+++ backend/src/categorias/dto/create-categoria.dto.ts
@@ -1,14 +1,18 @@
 import { IsNotEmpty, MaxLength, IsOptional, IsBoolean } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateCategoriaDto {
+  @ApiProperty({ example: 'Matemáticas', description: 'Nombre de la categoría académica', maxLength: 100 })
   @IsNotEmpty()
   @MaxLength(100)
   readonly CatNom: string;
 
+  @ApiPropertyOptional({ example: 'Recursos y libros relacionados con matemáticas', description: 'Descripción de la categoría académica' })
   @IsOptional()
   readonly CatDes?: string;
 
+  @ApiPropertyOptional({ example: true, description: 'Indica si la categoría está activa' })
   @IsOptional()
   @IsBoolean()
-  readonly LecAct?: boolean;
+  readonly CatAct?: boolean;
 }
