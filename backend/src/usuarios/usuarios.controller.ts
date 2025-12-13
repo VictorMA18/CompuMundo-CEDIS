1e1abfabb2703eca1587251b8d3b087bcd7bfcfd
VictorMA18 <vmamanian@unsa.edu.pe>
2025-12-13 18:24:02 -0500

    Documentando y ajustando usuarios

--- backend/src/usuarios/usuarios.controller.ts
+++ backend/src/usuarios/usuarios.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, NotFoundException, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, ParseIntPipe } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
@@ -22,28 +22,32 @@ export class UsuariosController {
     return await this.usuariosService.findAll();
   }
 
+  @Get('desactivados')
+  @Roles("administrador")
+  async findAllDesactivados() {
+    return await this.usuariosService.findAllDesactivados();
+  }
+
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const numId = Number(id);
-    if (isNaN(numId)) {
-      throw new BadRequestException('ID inválido');
-    }
-    const usuario = await this.usuariosService.findOne(numId);
-    if (!usuario) {
-      throw new NotFoundException('Usuario no encontrado');
-    }
-    return usuario;
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.usuariosService.findOne(id);
   }
 
   @Roles("administrador")
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
-    return await this.usuariosService.update(+id, updateUsuarioDto);
+  async update(@Param('id', ParseIntPipe) id: number, @Body() updateUsuarioDto: UpdateUsuarioDto) {
+    return await this.usuariosService.update(id, updateUsuarioDto);
+  }
+
+  @Roles("administrador")
+  @Patch('reactivar/:id')
+  async reactivar(@Param('id', ParseIntPipe) id: number) {
+    return await this.usuariosService.reactivar(id);
   }
 
   @Roles("administrador")
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return await this.usuariosService.remove(+id);
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return await this.usuariosService.remove(id);
   }
 }
